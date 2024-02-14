b5a5508755f47fec99d2696ca25e2a814454a7f6
hamoudi med khalil <mohamedkhalilhamoudi1@gmail.com>
2024-02-14 12:17:17 +0100

    Login & Register JSX

--- src/App.js
+++ src/App.js
@@ -2,14 +2,18 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from './pages/home/Home';
 import Header from './components/header/Header'
 import Footer from './components/footer/Footer'
+import Register from './pages/auth/Register';
+import Login from './pages/auth/Login';
 const App = () => {
   return (
     <>
       <BrowserRouter>
+
         <Header />
-        
         <Routes>
           <Route path='/' element={<Home/>} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/register' element={<Register />} />
         </Routes>
 
         <Footer />
--- src/components/carousel/CarouselItem.js
+++ src/components/carousel/CarouselItem.js
@@ -8,7 +8,7 @@ const CarouselItem = ({url, name, price, description}) => {
     <div className='carouselItem'>
          <Link to='/product-details'>
             <img className='product--image' src={url} alt={url} />
-            <p className='price'> {`$${price}`}</p>
+            <p className='price'> {`${price}`}</p>
             <h4> {shortenText(name, 18)}  </h4>
             <p className='--mb'> {shortenText(description, 26)} </p>
          </Link>
